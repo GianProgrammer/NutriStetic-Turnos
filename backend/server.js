62b192db8a040616f2ed8cb9679ad74e3a60a28e
GianProgrammer <gianferreyra2014@gmail.com>
2025-09-09 21:52:34 +0000

    password change

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ app.use(express.json());
 
 app.use("/turnos", turnosRoutes);
 
-mongoose.connect("mongodb+srv://gianferreyra2014_db_user:lozkfiIHrZutYYwk@bdturnos.as5xw9i.mongodb.net/?retryWrites=true&w=majority&appName=bdTurnos", {
+mongoose.connect("mongodb+srv://gianferreyra2014_db_user:<dbPassword>@bdturnos.as5xw9i.mongodb.net/?retryWrites=true&w=majority&appName=bdTurnos", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 })
